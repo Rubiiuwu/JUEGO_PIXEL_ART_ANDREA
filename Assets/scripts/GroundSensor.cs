6e35af22f03bbbb76ea39bdb4b4048675eff0e78
Rubiiuwu <andreacorteszamora@gmail.com>
2023-04-19 10:39:51 +0200

    JUEGO_PIXEL_ART_ANDREA

---
+++ Assets/scripts/GroundSensor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    private PlayerController controller;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controller = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other) 
+    {    
+       if (other.gameObject.layer == 0)
+       {
+        isGrounded = true;
+       }
+        else if (other.gameObject.layer == 0)
+       {
+        isGrounded = false;
+       }
+    }
+
+    void OnTriggerExit2D(Collider2D Other)
+    {
+        if(Other.gameObject.layer == 0)
+        {
+            isGrounded = false;
+        }
+    }
+
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float PlayerSpeed =15;
+    public float JumpForce =15;
+    public SpriteRenderer spriteRender;
+    private Rigidbody2D rBody;
+    private GroundSensor groundSensor; 
+    float horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender = GetComponent <SpriteRenderer>();
+        rBody = GetComponent <Rigidbody2D>();
+        groundSensor = GameObject.Find("GroundSensor").GetComponent <GroundSensor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if(horizontal<0)
+        {
+            spriteRender.flipX = true;
+        }
+        else if(horizontal>0)
+        {
+            spriteRender.flipX = false;
+        }
+
+         if(Input.GetButtonDown ("Jump") && groundSensor)
+        {
+            rBody.AddForce (Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+    void FixedUpdate() 
+    {
+     rBody.velocity = new Vector2 (horizontal * PlayerSpeed, rBody.velocity.y);
+    }
+}
